1f7cb37f40c72bc3b0322f1c1fc8e38be3c397b3
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2019-08-15 16:52:47 +0700

    done

--- app/src/main/java/com/example/drawroute/MapsActivity.kt
+++ app/src/main/java/com/example/drawroute/MapsActivity.kt
@@ -1,18 +1,33 @@
 package com.example.drawroute
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+@Suppress("UNREACHABLE_CODE")
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private var mMap: GoogleMap? = null
+    private lateinit var markerPoints: ArrayList<LatLng>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +36,107 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        markerPoints = ArrayList()
+
+    }
+
+    private fun getUrl(origin: LatLng, dest: LatLng): String {
+
+        // Origin of route
+        val strOrigin = "origin=" + origin.latitude + "," + origin.longitude
+
+        // Destination of route
+        val strDest: String = "destination=" + dest.latitude + "," + dest.longitude
+
+
+        // Sensor enabled
+        val sensor = "sensor=false"
+
+        // Building the parameters to the web service
+        val parameters = "$strOrigin&$strDest&$sensor"
+
+        // Output format
+        val output = "json"
+
+        // Building the url to the web service
+        return "https://maps.googleapis.com/maps/api/directions/$output?$parameters"
+    }
+
+
+    // Fetches data from url passed
+    @SuppressLint("StaticFieldLeak")
+    private inner class FetchUrl : AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg url: String): String {
+
+            // For storing data from web service
+            var data = ""
+
+            try {
+                // Fetching the data from web service
+                data = downloadUrl(url[0])
+                Log.d("Background Task data", data)
+            } catch (e: Exception) {
+                Log.d("Background Task", e.toString())
+            }
+
+            return data
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+            val parserTask = ParserTask()
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result)
+
+        }
+    }
+
+
+    @Throws(IOException::class)
+    private fun downloadUrl(strUrl: String): String {
+        var data = ""
+        var iStream: InputStream? = null
+        var urlConnection: HttpURLConnection? = null
+        try {
+            val url = URL(strUrl)
+
+            // Creating an http connection to communicate with url
+            urlConnection = url.openConnection() as HttpURLConnection
+
+            // Connecting to url
+            urlConnection.connect()
+
+            // Reading data from url
+            iStream = urlConnection.inputStream
+
+            val br = BufferedReader(InputStreamReader(iStream!!))
+
+            val sb = StringBuffer()
+
+            var line: String
+//            while ((line = br.readLine()) != null) {
+//                sb.append(line)
+//            }
+            while(true){
+                line = br.readLine()
+                sb.append(line)
+            }
+
+            data = sb.toString()
+            Log.d("downloadUrl", data)
+            br.close()
+
+        } catch (e: Exception) {
+            Log.d("Exception", e.toString())
+        } finally {
+            iStream!!.close()
+            urlConnection!!.disconnect()
+        }
+        return data
     }
 
     /**
@@ -37,7 +153,133 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+        mMap!!.setOnMapClickListener { point ->
+            // Already two locations
+            if (markerPoints.size > 1) {
+                markerPoints.clear()
+                mMap!!.clear()
+            }
+
+            // Adding new item to the ArrayList
+            markerPoints.add(point)
+
+            // Creating MarkerOptions
+            val options = MarkerOptions()
+
+            // Setting the position of the marker
+            options.position(point)
+
+            /**
+             * For the start location, the color of marker is GREEN and
+             * for the end location, the color of marker is RED.
+             */
+
+            /**
+             * For the start location, the color of marker is GREEN and
+             * for the end location, the color of marker is RED.
+             */
+            if (markerPoints.size == 1) {
+                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            } else if (markerPoints.size == 2) {
+                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+            }
+
+
+            // Add new marker to the Google Map Android API V2
+            mMap!!.addMarker(options)
+
+            // Checks, whether start and end locations are captured
+            if (markerPoints.size >= 2) {
+                val origin = markerPoints[0]
+                val dest = markerPoints[1]
+
+                // Getting URL to the Google Directions API
+                val url = getUrl(origin, dest)
+                Log.d("onMapClick", url)
+                val fetchUrl = FetchUrl()
+
+                // Start downloading json data from Google Directions API
+                fetchUrl.execute(url)
+                //move map camera
+                mMap!!.moveCamera(CameraUpdateFactory.newLatLng(origin))
+                mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
+            }
+        }
+
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    private inner class ParserTask : AsyncTask<String, Int, List<List<HashMap<String, String>>>>() {
+
+        // Parsing the data in non-ui thread
+        override fun doInBackground(vararg jsonData: String): List<List<HashMap<String, String>>> {
+
+            val jObject: JSONObject
+
+            try {
+                jObject = JSONObject(jsonData[0])
+                Log.d("ParserTask", jsonData[0])
+                val parser = DataParser()
+                Log.d("ParserTask", parser.toString())
+
+                // Starts parsing data
+                val routes: List<List<HashMap<String, String>>>  = parser.parse(jObject)
+                Log.d("ParserTask", "Executing routes")
+                Log.d("ParserTask", routes.toString())
+                return routes
+
+            } catch (e: Exception) {
+                Log.d("ParserTask", e.toString())
+                e.printStackTrace()
+            }
+
+            return ArrayList<ArrayList<HashMap<String, String>>>()
+        }
+
+        // Executes in UI thread, after the parsing process
+        override fun onPostExecute(result: List<List<HashMap<String, String>>>) {
+            var points: ArrayList<LatLng>
+            var lineOptions: PolylineOptions? = null
+
+            // Traversing through all the routes
+            for (i in result.indices) {
+                points = ArrayList()
+                lineOptions = PolylineOptions()
+
+                // Fetching i-th route
+                val path = result[i]
+
+                // Fetching all the points in i-th route
+                for (j in path.indices) {
+                    val point = path[j]
+
+                    val lat = java.lang.Double.parseDouble(point["lat"].toString())
+                    val lng = java.lang.Double.parseDouble(point["lng"].toString())
+                    val position = LatLng(lat, lng)
+
+                    points.add(position)
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points)
+                lineOptions.width(10f)
+                lineOptions.color(Color.RED)
+
+                Log.d("onPostExecute", "onPostExecute lineoptions decoded")
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            if (lineOptions != null) {
+                mMap!!.addPolyline(lineOptions)
+            } else {
+                Log.d("onPostExecute", "without Polyline drawn")
+            }
+        }
     }
 }
